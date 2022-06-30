68a8031df02a8be750647ae2519f6f3088787ca6
Joshua Blevins <josh.adam.blevins@gmail.com>
2022-06-30 01:13:37 -0400

    feat: recovered squashed about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import PageTemplate from "../templates/Page";
+import Link from "next/link";
 
 const AboutPage = () => {
     return <PageTemplate
@@ -13,30 +14,50 @@ const AboutPage = () => {
                     <h1>About Me</h1>
 
                     <p>
-                        I am a web application developer based out of Michigan. I have a
-                        permanent student mentality in that I am always honing my craft. I
-                        specialize in web accessibility and test-driven development.
+                        I started learning HTML and CSS back in 2013 when HTML 5 and CSS 3 were new
+                        standards and React was being developed in the depths of Facebook HQ. My
+                        first website was built using PHP 5.3 and WordPress. I decided to seek out work
+                        in software development in 2017 and landed an internship at a small manufacturing
+                        software development company. When I first heard about React.js I became
+                        fascinated with Web Development all over again.
                     </p>
 
                     <p>
-                        I have been writing React applications since 2018, ranging from smaller
-                        businesses to large enterprise applications. Some of these companies
-                        include Ford Motor Company, Lowes Home Improvement, and AAA.
+                        I slowly built up my skill set and started a contract in 2019 with Ford Motor Company
+                        on their Single-Sign On team where I started working with Typescript and learned about
+                        Test-Driven Development and grew a lot as a developer. I owned multiple features like the
+                        password reset flow, content management integration, data layer integration, and hosted
+                        many office hours for teams that integrate with the single-sign on library.
                     </p>
 
                     <p>
-                        I have been on a large variety of projects throughout my career. This
-                        includes greenfield web applications, matured web applications, node.js
-                        module libraries, and full-stack projects. I have had the pleasure of
-                        taking responsibility and ownership of entire features during this time,
-                        including analytics data layer implementation, performance testing and
-                        monitoring, search-engine optimization, web accessibility, and business
-                        logic.
+                        From here I moved on to Lowes Home Improvement, where I worked on their Design
+                        System team. It was here I learned about the aria authoring practices and worked
+                        closely with designers. I brought atomic components to life within design and
+                        web accessibility specifications. We documented our design system and style guide
+                        using Gatsby.
+                    </p>
+
+                    <p>
+                        Another project that I learned a lot from is Clublabs of AAA. I was the technical
+                        lead for the Design System team, where we prototyped and developed out their
+                        component library. I introduced design system and style guide standards and
+                        agile software development practices and prototyped Webpack Module Federation.
+                    </p>
+
+                    <p>
+                        My favorite thing about web application development is creating something
+                        with nothing but time and an idea. I like the challenges that I have to
+                        overcome to making things work. My least favorite thing about being a
+                        developer is not writing automated tests. I err on the lazy side and
+                        testing tens of workflows manually does not sound like a fun or
+                        productive use of my time when I can code a test.
                     </p>
 
                     <p>
                         {/* eslint-disable-next-line react/no-unescaped-entities */}
-                        This blog's <a href={'https://github.com/jblevins1991/joshuablevins-next'}>frontend</a> and <a href={''}>backend</a> code is open-source under the GPT-3 license.
+                        This blog's <Link href={'https://github.com/jblevins1991/joshuablevins-next'}><a rel={'nofollow'} target={'_blank'}>frontend</a></Link>
+                        and <Link href={''}><a rel={'nofollow'} target={'_blank'}>backend</a></Link> code is open-source under the GPT-3 license.
                     </p>
                 </main>
 
@@ -71,6 +92,13 @@ const AboutPage = () => {
                     </ul>
                 </aside>
             </div>
+
+            {/*  @todo: add helpful links (github, acloudguru, linkedin, etc.)  */}
+            <button onClick={() => {
+                throw new Error("Sentry Frontend Error")
+            }}>
+                Throw error
+            </button>
         </main>
     </PageTemplate>;
 }
