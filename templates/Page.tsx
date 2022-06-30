335a55ef41614f72d14a8b8e90b2e9726de273b4
Joshua Blevins <josh.adam.blevins@gmail.com>
2022-06-30 00:34:47 -0400

    fix: added useRouter import back

--- templates/Page.tsx
+++ templates/Page.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Head from 'next/head';
+import {useRouter} from "next/router";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
